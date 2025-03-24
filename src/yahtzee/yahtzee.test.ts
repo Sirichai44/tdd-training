d3ea70724eaf3d935c76cfe568128a837ff854e5
Sirichai <mercier_1999@hotmail.com>
2025-03-24 11:44:28 +0700

    function and test of lower section

--- src/yahtzee/yahtzee.test.ts
+++ src/yahtzee/yahtzee.test.ts
@@ -1,9 +1,9 @@
-import { sumScoreByTarget } from "./yahtzee";
+import { Dices, Kind, Straight, sumScoreByTarget, valueOfChance, valueOfFullHouse, valueOfKind, valueOfStraight, valueOfYahtzee } from "./yahtzee";
 
 describe("Sum Score by Target > Success Case", () => {
   describe("Target is 1", () => {
     it("Should be return 0", () => {
-      const input = [6,5,2,3,3]
+      const input:Dices = [6,5,2,3,3]
       const target = 1
       const expected = 0
   
@@ -13,7 +13,7 @@ describe("Sum Score by Target > Success Case", () => {
     })
 
     it("Should be return 1", () => {
-      const input = [2,3,3,3,1]
+      const input:Dices = [2,3,3,3,1]
       const target = 1
       const expected = 1
   
@@ -23,7 +23,7 @@ describe("Sum Score by Target > Success Case", () => {
     })
 
     it("Should be return 2", () => {
-      const input = [2,1,3,2,1]
+      const input:Dices = [2,1,3,2,1]
       const target = 1
       const expected = 2
   
@@ -35,7 +35,7 @@ describe("Sum Score by Target > Success Case", () => {
 
   describe("Target is 2", () => {
     it("Should be return 0", () => {
-      const input = [6,5,1,3,3]
+      const input:Dices = [6,5,1,3,3]
       const target = 2
       const expected = 0
   
@@ -45,7 +45,7 @@ describe("Sum Score by Target > Success Case", () => {
     })
 
     it("Should be return 2", () => {
-      const input = [2,3,3,3,1]
+      const input:Dices = [2,3,3,3,1]
       const target = 2
       const expected = 2
   
@@ -55,7 +55,7 @@ describe("Sum Score by Target > Success Case", () => {
     })
 
     it("Should be return 4", () => {
-      const input = [2,1,3,2,1]
+      const input:Dices = [2,1,3,2,1]
       const target = 2
       const expected = 4
   
@@ -67,7 +67,7 @@ describe("Sum Score by Target > Success Case", () => {
 
   describe("Target is 2", () => {
     it("Should be return 0", () => {
-      const input = [6,5,1,3,3]
+      const input:Dices = [6,5,1,3,3]
       const target = 2
       const expected = 0
   
@@ -77,7 +77,7 @@ describe("Sum Score by Target > Success Case", () => {
     })
 
     it("Should be return 2", () => {
-      const input = [2,3,3,3,1]
+      const input:Dices = [2,3,3,3,1]
       const target = 2
       const expected = 2
   
@@ -87,7 +87,7 @@ describe("Sum Score by Target > Success Case", () => {
     })
 
     it("Should be return 4", () => {
-      const input = [2,1,3,2,1]
+      const input:Dices = [2,1,3,2,1]
       const target = 2
       const expected = 4
   
@@ -99,7 +99,7 @@ describe("Sum Score by Target > Success Case", () => {
 
   describe("Target is 3", () => {
     it("Should be return 0", () => {
-      const input = [6,5,1,2,2]
+      const input:Dices = [6,5,1,2,2]
       const target = 3
       const expected = 0
   
@@ -109,7 +109,7 @@ describe("Sum Score by Target > Success Case", () => {
     })
 
     it("Should be return 3", () => {
-      const input = [2,3,2,2,1]
+      const input:Dices = [2,3,2,2,1]
       const target = 3
       const expected = 3
   
@@ -119,7 +119,7 @@ describe("Sum Score by Target > Success Case", () => {
     })
 
     it("Should be return 6", () => {
-      const input = [2,1,3,3,1]
+      const input:Dices = [2,1,3,3,1]
       const target = 3
       const expected = 6
   
@@ -131,7 +131,7 @@ describe("Sum Score by Target > Success Case", () => {
 
   describe("Target is 4", () => {
     it("Should be return 0", () => {
-      const input = [6,5,1,2,2]
+      const input:Dices = [6,5,1,2,2]
       const target = 4
       const expected = 0
   
@@ -141,7 +141,7 @@ describe("Sum Score by Target > Success Case", () => {
     })
 
     it("Should be return 4", () => {
-      const input = [2,4,2,2,1]
+      const input:Dices = [2,4,2,2,1]
       const target = 4
       const expected = 4
   
@@ -151,7 +151,7 @@ describe("Sum Score by Target > Success Case", () => {
     })
 
     it("Should be return 8", () => {
-      const input = [2,1,4,4,1]
+      const input:Dices = [2,1,4,4,1]
       const target = 4
       const expected = 8
   
@@ -163,7 +163,7 @@ describe("Sum Score by Target > Success Case", () => {
 
   describe("Target is 5", () => {
     it("Should be return 0", () => {
-      const input = [6,6,2,3,3]
+      const input:Dices = [6,6,2,3,3]
       const target = 5
       const expected = 0
   
@@ -173,7 +173,7 @@ describe("Sum Score by Target > Success Case", () => {
     })
 
     it("Should be return 10", () => {
-      const input = [2,5,5,2,1]
+      const input:Dices = [2,5,5,2,1]
       const target = 5
       const expected = 10
   
@@ -183,7 +183,7 @@ describe("Sum Score by Target > Success Case", () => {
     })
 
     it("Should be return 15", () => {
-      const input = [5,6,5,3,5]
+      const input:Dices = [5,6,5,3,5]
       const target = 5
       const expected = 15
   
@@ -195,7 +195,7 @@ describe("Sum Score by Target > Success Case", () => {
 
   describe("Target is 6", () => {
     it("Should be return 0", () => {
-      const input = [1,5,2,3,3]
+      const input:Dices = [1,5,2,3,3]
       const target = 6
       const expected = 0
   
@@ -205,7 +205,7 @@ describe("Sum Score by Target > Success Case", () => {
     })
 
     it("Should be return 12", () => {
-      const input = [5,6,5,6,5]
+      const input:Dices = [5,6,5,6,5]
       const target = 6
       const expected = 12
   
@@ -215,7 +215,7 @@ describe("Sum Score by Target > Success Case", () => {
     })
 
     it("Should be return 24", () => {
-      const input = [6,6,6,6,1]
+      const input:Dices = [6,6,6,6,1]
       const target = 6
       const expected = 24
   
@@ -229,49 +229,273 @@ describe("Sum Score by Target > Success Case", () => {
 });
 
 describe("Sum Score by Target > Alternative Case", () => {
-  describe("Dice array length must be 5 long", () => {
+  describe.skip("[Handle by Typescript] Dice array length must be 5 long", () => {
     it("Dice array is empty", () => {
-      const input: number[] = []
-      const target = 1
-      const expected = "Dice array length must be 5 long"
+      // const input: number[] = []
+      // const target = 1
+      // const expected = "Dice array length must be 5 long"
 
-      const result = sumScoreByTarget(input, target)
+      // const result = sumScoreByTarget(input, target)
 
-      expect(result).toEqual(expected)
+      // expect(result).toEqual(expected)
     });
 
     it("Dice array length is 6", () => {
-      const input = [1,2,3,4,5,6]
-      const target = 1
-      const expected = "Dice array length must be 5 long"
+      // const input = [1,2,3,4,5,6]
+      // const target = 1
+      // const expected = "Dice array length must be 5 long"
 
-      const result = sumScoreByTarget(input, target)
+      // const result = sumScoreByTarget(input, target)
 
-      expect(result).toEqual(expected)
+      // expect(result).toEqual(expected)
     })
   });
 
-  describe("Number in dice is not in range 1-6", () => {
+  describe.skip("[Handle by Typescript] Number in dice is not in range 1-6", () => {
     it("Some number in dice out of range", () => {
-      const input = [7,7,1,1,1]
-      const target = 1
-      const expected = "Number in dice is not in range 1-6"
+      // const input = [7,7,1,1,1]
+      // const target = 1
+      // const expected = "Number in dice is not in range 1-6"
 
-      const result = sumScoreByTarget(input, target)
+      // const result = sumScoreByTarget(input, target)
 
-      expect(result).toEqual(expected)
+      // expect(result).toEqual(expected)
     })
   });
 
-  describe("Target is not in range 1-6", () => {
+  describe.skip("[Handle by Typescript] Target is not in range 1-6", () => {
     it("Target is 7", () => {
-      const input = [1,1,1,1,1]
-      const target = 7
-      const expected = "Target must between 1-6"
+      // const input = [1,1,1,1,1]
+      // const target = 7
+      // const expected = "Target must between 1-6"
 
-      const result = sumScoreByTarget(input, target)
+      // const result = sumScoreByTarget(input, target)
 
-      expect(result).toEqual(expected)
+      // expect(result).toEqual(expected)
     })
   });
 });
+
+describe("Three of a Kind > Success Case", () => {
+  it("Should be return 0", () => {
+    const input:Dices = [1,2,3,4,5]
+    const kind:Kind = 3
+    const expected = 0
+
+    const result = valueOfKind(input, kind)
+
+    expect(result).toEqual(expected)
+  });
+
+  it("Should be return 12", () => {
+    const input:Dices = [2,3,3,3,1]
+    const kind:Kind = 3
+    const expected = 12
+
+    const result = valueOfKind(input, kind)
+
+    expect(result).toEqual(expected)
+  })
+
+  it("Should be return 25", () => {
+    const input:Dices = [6,6,6,6,1]
+    const kind:Kind = 3
+    const expected = 25
+
+    const result = valueOfKind(input, kind)
+
+    expect(result).toEqual(expected)
+  })
+});
+
+describe("Four of a Kind > Success Case", () => {
+  it("Should be return 0", () => {
+    const input:Dices = [1,2,1,1,3]
+    const kind:Kind = 4
+    const expected = 0
+
+    const result = valueOfKind(input, kind)
+
+    expect(result).toEqual(expected)
+  });
+
+  it("Should be return 20", () => {
+    const input:Dices = [4,4,4,4,4]
+    const kind:Kind = 4
+    const expected = 20
+
+    const result = valueOfKind(input, kind)
+
+    expect(result).toEqual(expected)
+  })
+
+  it("Should be return 23", () => {
+    const input:Dices = [5,5,3,5,5]
+    const kind:Kind = 4
+    const expected = 23
+
+    const result = valueOfKind(input, kind)
+
+    expect(result).toEqual(expected)
+  })
+})
+
+describe("Three | Four of a Kind > Alternative Case", () => {
+  it.skip("[Handle by Typescript] Kind must be a number", () => {})
+  it.skip("[Handle by Typescript] Kind must be 3 or 4", () => {})
+})
+
+describe("Full House > Success Case", () => {
+  it("Should be return 0", () => {
+    const input:Dices = [1,2,1,4,3]
+    const expected = 0
+
+    const result = valueOfFullHouse(input)
+
+    expect(result).toEqual(expected)
+  });
+
+  it("Should be return 25", () => {
+    const input:Dices = [4,4,4,5,5]
+    const expected = 25
+
+    const result = valueOfFullHouse(input)
+
+    expect(result).toEqual(expected)
+  })
+
+  it("Should be return 25", () => {
+    const input:Dices = [5,5,3,3,5]
+    const expected = 25
+
+    const result = valueOfFullHouse(input)
+
+    expect(result).toEqual(expected)
+  })
+})
+
+describe("Small Straight > Success Case", () => {
+  it("Should be return 0", () => {
+    const input:Dices = [1,1,3,4,6]
+    const straight:Straight = 'SmallStraight'
+    const expected = 0
+
+    const result = valueOfStraight(input, straight)
+
+    expect(result).toEqual(expected)
+  })
+
+  it("Should be return 30", () => {
+    const input:Dices = [1,2,3,4,6]
+    const straight:Straight = 'SmallStraight'
+    const expected = 30
+
+    const result = valueOfStraight(input, straight)
+
+    expect(result).toEqual(expected)
+  })
+
+  it("Should be return 30", () => {
+    const input:Dices = [3,2,3,4,5]
+    const straight:Straight = 'SmallStraight'
+    const expected = 30
+
+    const result = valueOfStraight(input, straight)
+
+    expect(result).toEqual(expected)
+  })
+})
+
+describe("Large Straight > Success Case", () => {
+  it("Should be return 0", () => {
+    const input:Dices = [1,1,3,4,6]
+    const straight:Straight = 'LargeStraight'
+    const expected = 0
+
+    const result = valueOfStraight(input, straight)
+
+    expect(result).toEqual(expected)
+  })
+
+  it("Should be return 40", () => {
+    const input:Dices = [1,2,3,4,5]
+    const straight:Straight = 'LargeStraight'
+    const expected = 40
+
+    const result = valueOfStraight(input, straight)
+
+    expect(result).toEqual(expected)
+  })
+
+  it("Should be return 40", () => {
+    const input:Dices = [6,3,4,2,5]
+    const straight:Straight = 'LargeStraight'
+    const expected = 40
+
+    const result = valueOfStraight(input, straight)
+
+    expect(result).toEqual(expected)
+  })
+})
+
+describe("Small | Large Straight > Alternative Case", () => {
+  it.skip("[Handle by Typescript] Straight must be a 'SmallStraight' or 'LargeStraight'", () => {})
+})
+
+describe("Chance > Success Case", () => {
+  it("Should be return 15", () => {
+    const input:Dices = [1,1,3,4,6]
+    const expected = 15
+
+    const result = valueOfChance(input)
+
+    expect(result).toEqual(expected)
+  });
+
+  it("Should be return 18", () => {
+    const input:Dices = [2,2,5,4,5]
+    const expected = 18
+
+    const result = valueOfChance(input)
+
+    expect(result).toEqual(expected)
+  })
+
+  it("Should be return 19", () => {
+    const input:Dices = [5,2,2,4,6]
+    const expected = 19
+
+    const result = valueOfChance(input)
+
+    expect(result).toEqual(expected)
+  })
+})
+
+describe("Yahtzee > Success Case", () => {
+  it("Should be return 0", () => {
+    const input:Dices = [1,1,3,4,6]
+    const expected = 0
+
+    const result = valueOfYahtzee(input)
+
+    expect(result).toEqual(expected)
+  })
+
+  it("Should be return 50", () => {
+    const input:Dices = [1,1,1,1,1]
+    const expected = 50
+
+    const result = valueOfYahtzee(input)
+
+    expect(result).toEqual(expected)
+  })
+
+  it("Should be return 50", () => {
+    const input:Dices = [3,3,3,3,3]
+    const expected = 50
+
+    const result = valueOfYahtzee(input)
+
+    expect(result).toEqual(expected)
+  })
+})
--- src/yahtzee/yahtzee.ts
+++ src/yahtzee/yahtzee.ts
@@ -1,7 +1,83 @@
-export const sumScoreByTarget = (input: number[], target: number) => {
-  if(input.length !== 5) return "Dice array length must be 5 long"
-  if(input.some(dice => dice < 1 || dice > 6)) return "Number in dice is not in range 1-6"
-  if(target < 1 || target > 6) return "Target must between 1-6"
+export type Kind = 3 | 4
+export type Straight = "SmallStraight" | "LargeStraight"
+export type Dice = 1 | 2 | 3 | 4 | 5 | 6
+export type Dices = [Dice, Dice, Dice, Dice, Dice]
 
-  return input.reduce((sum, dice) => dice === target ? sum + dice : sum, 0)
+export const sumScoreByTarget = (input: Dices, target: Dice) => {
+  // if(input.length !== 5) return "Dice array length must be 5 long"
+  // if(input.some(dice => dice < 1 || dice > 6)) return "Number in dice is not in range 1-6"
+  // if(target < 1 || target > 6) return "Target must between 1-6"
+  let sum = 0
+  for (const dice of input) {
+    if (dice === target) {
+      sum += dice
+    }
+  }
+
+  return sum
+}
+
+const countDice = (input: Dices) => {
+  const countDice = Array(6).fill(0)
+  for (const dice of input) {
+    countDice[dice - 1]++
+  }
+
+  return countDice
+}
+
+export const valueOfKind = (input: Dices, kind:Kind) => {
+  const count = countDice(input)
+
+  const ofKind = count.some(count => count >= kind)
+  if (!ofKind) return 0
+
+  let sum = 0
+  for (let i = 0; i < count.length; i++) {
+    sum += (i + 1) * count[i]
+  }  
+  
+  return sum
+}
+
+export const valueOfFullHouse = (input: Dices) => {
+  const count = countDice(input)
+
+  const threeOfKind = count.some(count => count === 3)
+  const twoOfKind = count.some(count => count === 2)
+
+  if (threeOfKind && twoOfKind) {
+    return 25
+  }
+  return 0
+}
+
+export const valueOfStraight = (input: Dices, arrange:Straight) => {
+  const sortDice = input.sort((a, b) => a - b)
+
+  let straight = 1
+  for (let i = 0; i < sortDice.length - 1; i++) {
+    if (sortDice[i] + 1 === sortDice[i + 1]) {
+      straight++
+    }
+  }
+
+  if(straight < 4) return 0
+  if (arrange === "SmallStraight" && straight === 4) return 30
+  if (arrange === "LargeStraight" && straight === 5) return 40
+}
+
+export const valueOfChance = (input: Dices) => {
+  let sum = 0
+  for (const dice of input) {
+    sum += dice
+  }
+  return sum
+}
+
+export const valueOfYahtzee = (input: Dices) => {
+  const count = countDice(input)
+  const yahtzee = count.some(count => count === 5)
+
+  return yahtzee ? 50 : 0
 }
